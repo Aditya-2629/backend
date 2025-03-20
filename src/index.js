39b5762981077a32582c0ebeb3d511313e73b7db
sujal1028 <138969035+sujal1028@users.noreply.github.com>
2025-03-21 00:34:25 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/dbConnect.js";
 
-connectDB();
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`👀👀 server is running at port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(" ❌ mongo db connection failed !! : ", err);
+  });
