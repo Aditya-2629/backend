78e5315d24b5271a7afd5f48a090e8191cf06f91
sujal1028 <138969035+sujal1028@users.noreply.github.com>
2025-03-21 18:27:24 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,16 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//! <============= routes import =============>
+
+import userRouter from "./routes/user.routes.js";
+
+//*  <============== routes declaration ==============>
+
+app.use("/api/v1/users", userRouter); // ye actualy karega ye http://localhost:8000/api/v1/user or uske userRouter ke pass power jayega ab vaha kya call ho raha hai vese url banta jayega
+
+//*  <============== routes declaration ==============>
+
+//! <============= routes import =============>
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/dbConnect.js";
+import { app } from "./app.js";
 
 const port = process.env.PORT || 3000;
 
